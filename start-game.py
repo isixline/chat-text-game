d7d708016ce79ad00c4368bb817f8070dfa45b88
isixline <isixline@qq.com>
2023-04-21 08:22:23 +0800

    feat: add prompt

--- start-game.py
+++ start-game.py
@@ -20,13 +20,21 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 # choiceIndex = input("请随机选择一个随机游戏背景：")
 # choice = response.choices[int(choiceIndex) - 1].text.strip()
 
-choice = '《未来之城》：游戏者是一名在未来城市生活的居民，这座城市拥有许多超级科技和令人惊叹的景观。但是，一些神秘的事件开始发生，使城市的秩序陷入混乱，游戏者需要与其他人一起合作，揭开这些神秘事件背后的真相并拯救这座城市。'
+backGroud = '《未来之城》：小明是一名在未来城市生活的居民，这座城市拥有许多超级科技和令人惊叹的景观。但是，一些神秘的事件开始发生，使城市的秩序陷入混乱，小明需要与其他人一起合作，揭开这些神秘事件背后的真相并拯救这座城市。'
+nextStepFormat = {"current": "当前情节", "options": ["选项1", "选项2", "选项3"]}
+systemSetUp = f'你是一个交互式文本游戏引擎。游戏背景为{backGroud}。你的回答为json格式，格式参考{nextStepFormat}'
 
-jsonFormat = '{"options": [{"number": "选项序号", "content":"选项内容"}]}'
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": f"你是一个交互式文本游戏引擎。\n游戏背景为{choice}\n需要你为后续情节的发展提供选项，回答使用json格式{jsonFormat}"},
+messages=[
+        {'role': 'system', 'content': systemSetUp},
+        {'role': 'user', 'content': '你好，我是小明，现在开始游戏'}
     ]
+
+response = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages=messages,
 )
-print(response.choices[0].message.content)
+
+nextStep = response.choices[0].message.content
+print(nextStep.current)
+print(nextStep.options)
+
